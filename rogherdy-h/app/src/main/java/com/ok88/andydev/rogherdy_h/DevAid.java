f55a494598be2a173783b4057319efb6acc7730a
Johnston <jcgh00@okdaily.com>
2015-05-13 12:52:12 -0700

    Add DevAid.java with getAssetTextData()

---
+++ rogherdy-h/app/src/main/java/com/ok88/andydev/rogherdy_h/DevAid.java
@@ -0,0 +1,37 @@
+//5-13-15  JChoy Support classes for MainActivity
+
+
+package com.ok88.andydev.rogherdy_h;
+
+import java.io.*;
+import java.io.InputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+
+
+//
+// AppAid
+//
+public class DevAid extends Object {
+	//
+	// getAssetTextData
+	//
+	public static String[] getAssetTextData(Context ctx, String filename)
+	throws IOException
+	{
+		InputStream input = ctx.getAssets().open(filename,1);
+		int size = input.available();
+		
+		if (size>0){	
+			byte[] buffer = new byte[size];
+			input.read(buffer);
+			input.close();
+
+			String text = new String(buffer);
+			return text;
+		} 
+		return null;
+	}
+}//class
+
