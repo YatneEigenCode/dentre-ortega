73ade8a5f5945ae534f6c7dbffd7728e7bfc1bab
Johnston <jcgh00@okdaily.com>
2015-07-28 14:49:39 -0700

    logsum.js.bat - Added saad: SaadFactory, SaadMozDiv

--- guptinal-i/saad/logsum.js.bat
+++ guptinal-i/saad/logsum.js.bat
@@ -1,5 +1,5 @@
 /*
-rem 7/27/2015 JChoy catalog new log files in folder and prep for quick viewing
+rem 7/28/2015 JChoy catalog new log files in folder and prep for quick viewing
 set folder=C:\abc\logs
 if not exist %~n0.txt echo.>%~n0.txt
 echo new items>%~n0_new.txt
@@ -9,6 +9,8 @@ for /f %%A in ('dir /b %folder%\*') do (
 for /f %%A in (%~n0_new.txt) do (
 	type %folder%\%%A.00 | cscript //nologo //E:JScript %0 %%A /parse >> %~n0.txt
 )
+sort %~n0.txt | cscript //nologo //E:JScript %0 /saad > %~n0.html
+pause
 exit(0)
 */
 
@@ -67,16 +69,58 @@ function FilterWig( fn ){
     }
 }
 //-----
+function SaadFactory(startCmd){
+	this.startCmd= startCmd;
+	this.genCode= function(acn){
+		var at = ["<script>"];
+		for (var i=0; i<acn.length; i++)
+			at.push(acn[i]+"="+eval(acn[i]).toString());
+		at.push(this.startCmd);
+		at.push("</script>");
+		return at.join("\n");
+	}
+}
+//-----
+SaadMozDiv=function SaadMozDiv(){
+  this.setupDivs= function(){
+	var d$= document;
+	this.dataDiv= d$.getElementsByTagName("div")[0];
+	this.uiDiv= d$.body.appendChild(d$.createElement("div"));
+	this.uiDiv.innerHTML= "Welcome";
+	this.dataDiv.style.display= "none";
+  }
+}
+//Jobs, errors, last10, search, count, range
+SaadMozDiv1=function SaadMozDiv1(){
+  this.constructor= SaadMozDiv;
+  this.constructor();
+  this.start= function(){
+	this.setupDivs();
+  }
+}
+
+//-----
 //for (var i=0; i<WScript.Arguments.length; i++) WScript.Echo( i+": "+WScript.Arguments(i) );
-if (WScript.Arguments(1)=="/filter"){
+if (WScript.Arguments.length>1) {
+    if (WScript.Arguments(1)=="/filter"){
 	var fw = new FilterWig( WScript.Arguments(0) );
 	new StdInLooper( fw ).start( "check" );
 	if (fw.getResult()) WScript.echo( fw.getResult() );
-}
-if (WScript.Arguments(1)=="/parse"){
+    }
+    if (WScript.Arguments(1)=="/parse"){
 	var pw = new ParserWig( WScript.Arguments(0) );
 	if (pw.target!="new") {
 		new StdInLooper( pw ).start( "parse" );
 		WScript.echo( pw.getResult() );
 	}
+    }
+}
+if (WScript.Arguments(0)=="/saad"){
+	WScript.echo( "<body><div>" );
+	while (!WScript.StdIn.AtEndOfStream) 
+		WScript.echo( WScript.StdIn.ReadLine() );
+	WScript.echo( "</div></body>" );
+	var cnCsv="SaadMozDiv,SaadMozDiv1"
+	WScript.echo( new SaadFactory("new SaadMozDiv1().start()").genCode(
+		["SaadMozDiv","SaadMozDiv1"]) );
 }
