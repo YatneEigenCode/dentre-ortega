24e30edd851810cea635c7b8746fb4f4359a3218
Johnston <jcgh00@okdaily.com>
2015-08-04 15:29:17 -0700

    loopwithlimit.js.bat - echLast10(), checkArg()

--- guptinal-i/saad/loopwithlimit.js.bat
+++ guptinal-i/saad/loopwithlimit.js.bat
@@ -3,20 +3,25 @@ rem 8-4-2015 JChoy loopwithlimit.js.bat - converting a long for loop to a short 
 set folder=c:\Windows
 
 echo.>%~n0.txt
-for /f %%A in ('"dir /b /o:d /a:-d %folder%\* | cscript //nologo //e:Jscript %0 /last10"') do echo %%A >>%~n0.txt
+for /f %%A in ('"dir /b /o:d /a:-d %folder%\* | cscript //nologo //e:Jscript %0 /last10 25"') do echo %%A >>%~n0.txt
 pause
 */
-if (WScript.Arguments.length==0) {
-} else if (WScript.Arguments(0)=="/last10") {
-	var zbuf = 10
-	if (WScript.Arguments.length>1) {
-		var z1 = parseInt(WScript.Arguments(1));
-		if (!isNaN(z1)) zbuf= z1;
-	}
+//----
+function checkArg( num, val ){
+	if (WScript.Arguments.length <= num) return false;
+	if (val) return (WScript.Arguments(num) == val);
+	return WScript.Arguments(num);
+}
+function echoLast10() {
+	var z1 = parseInt(checkArg(1));
+	var zbuf = (isNaN(z1))? 10 : z1;
 	var at=new Array(zbuf+1);
 	for (var i=0; (!WScript.Stdin.AtEndOfStream); i++){
 		at[zbuf]= WScript.Stdin.readLine();
 		at.shift();
 	}
 	WScript.echo( at.join("\n") );
-}
+}//fcn
+
+if (checkArg( 0, "/last10" )) echoLast10();
+
