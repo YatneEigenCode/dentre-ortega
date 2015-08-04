e2c003e894bf31c3de724ccdc78aca1089ef0073
Johnston <jcgh00@okdaily.com>
2015-08-04 15:37:01 -0700

    loopwithlimit.js.bat - streamline code

--- guptinal-i/saad/loopwithlimit.js.bat
+++ guptinal-i/saad/loopwithlimit.js.bat
@@ -1,11 +1,12 @@
 /*
-rem 8-4-2015 JChoy loopwithlimit.js.bat - converting a long for loop to a short loop using js
+@echo off
+rem 8-4-2015 JChoy loopwithlimit.js.bat - converting a long for-loop to a short loop using js
 set folder=c:\Windows
 
-echo.>%~n0.txt
-for /f %%A in ('"dir /b /o:d /a:-d %folder%\* | cscript //nologo //e:Jscript %0 /last10 25"') do echo %%A >>%~n0.txt
+for /f %%A in ('"dir /b /o:d /a:-d %folder%\* | cscript //nologo //e:Jscript %0 /last10 25"') do echo %%A
 pause
 */
+
 //----
 function checkArg( num, val ){
 	if (WScript.Arguments.length <= num) return false;
@@ -15,11 +16,9 @@ function checkArg( num, val ){
 function echoLast10() {
 	var z1 = parseInt(checkArg(1));
 	var zbuf = (isNaN(z1))? 10 : z1;
-	var at=new Array(zbuf+1);
-	for (var i=0; (!WScript.Stdin.AtEndOfStream); i++){
+	var at=new Array(zbuf + 1);
+	for (var i=0; (!WScript.Stdin.AtEndOfStream); at.shift(),i++)
 		at[zbuf]= WScript.Stdin.readLine();
-		at.shift();
-	}
 	WScript.echo( at.join("\n") );
 }//fcn
 
