eee446d5163cdea980b82df3b9c9080221b0aec1
Johnston <jcgh00@okdaily.com>
2015-10-10 14:10:17 -0700

    added mr6.jpg for fb to assume mimetype

--- guptinal-i/gooclpython/appserve.py
+++ guptinal-i/gooclpython/appserve.py
@@ -4,6 +4,7 @@ from haqabob import (Haqabob, Manifest, TextStore)
 from webappWform import (HelloWebapp2, AppWform)
 from mimerelay import (MimeText,MimeText64,MimeTextSave,MimeTextSave64,MimeRelay,MimeRelay64)
 
+# 10-10-2015 JChoy added mr6.jpg for fb to assume mimetype
 # appserve.py 9-10-2015 v0.115 JChoy use webapp2 to serve python webapp from py imports
 # this works, but you need to run "sudo easy_install webapp2" in c9 terminal once.
 
@@ -19,6 +20,8 @@ app = webapp2.WSGIApplication([
     ('/mbs64', MimeTextSave64),
     ('/mimerelay', MimeRelay),
     ('/mimerelay64', MimeRelay64),
+    ('/mimerelay64.jpg', MimeRelay64),
+    ('/mr6.jpg', MimeRelay64),
 ], debug=True)
 
 def main():
