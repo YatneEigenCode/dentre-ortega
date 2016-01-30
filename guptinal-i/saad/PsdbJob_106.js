a4bdaef573d06e220ddb6a574ed68fca888bd403
Johnston <jcgh00@okdaily.com>
2016-01-30 10:55:27 -0800

    PsdbJob_106.js separate file from hta

---
+++ guptinal-i/saad/PsdbJob_106.js
@@ -0,0 +1,26 @@
+//1-30-2016 JChoy PsdbJob_106.js separate file from hta
+//copies ts source code into dropbox tssrc folder
+//-----
+function PsdbJob_106(){
+    this.msf = new MSFeatures();
+    if (PageAppCfg) new PageAppCfg().config(this);
+    this.path = "c:\\Users\\choy\\Dropbox\\Public\\jsondata\\psdb\\ape\\tssrc\\";
+    this.ajax= new Ajax();
+    var pt= this;
+    this.ajax.write= function(s){ pt.writeFile(s) }
+    this.numList= [2327,2326];
+    this.start= function(){
+      if (this.numList.length <= 0) return;
+      this.cNum = this.numList.pop();
+      this.fn = this.cNum+".txt";
+      var url= this.tsUrl +"?f=text&i="+this.cNum;
+      this.ajax.webGet( url );
+    }
+    this.writeFile= function(s){
+      if (!pt.fn) return;
+      var fo = this.msf.fso.openTextFile( pt.path+pt.fn, 2, true );
+      fo.write( s );
+      fo.close();
+      pt.start();
+    }
+}
