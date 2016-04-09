43d0bf4a076d57dbcc9dde51aa6dad179d900021
Johnston <jcgh00@okdaily.com>
2016-04-09 10:08:14 -0700

    split into 2 files FavTrak.js and QuikNote.js

---
+++ guptinal-i/saad/js/FavTrak.js
@@ -0,0 +1,101 @@
+//4-8-2016 jchoy RedButton, AutoLink, AutoSave, FavTrak
+//https://raw.githubusercontent.com/douglascrockford/JSON-js/master/json2.js
+//JsonLoader is in ts2326
+//-----
+function AutoLink(){
+	(function(t,c,a){t.c=c,t.c(a)})(this,FormHelper);
+	var $ea;
+	this.start= function( s, par, mgr ){
+		this.massage(s, par);
+		var frm= this.addEl( "form", par );
+		this.addEl( "button", frm ).value="Edit";
+		this.onButtonClick= mgr.onButtonClick;
+		this.prepForm( frm );
+		frm.helper= mgr;
+	}
+	this.massage= function( s, el ){
+		for (var m in $ea=this.enumArray(s.split("\n"))){
+			this._= new FavTrak().start( this.addEl("div", el), m, $ea[m].length );
+			this.addEl("a", this._.parentNode).innerHTML = $ea[m];
+			this._.href= $ea[m];
+			this._.parentNode.style.marginTop= "6";
+		}
+	}
+}
+//-----
+function FavTrak(){
+	(function(t,c,a){t.c=c,t.c(a)})(this,FormHelper);
+	(function(t,c,a){t.c=c,t.c(a)})(this,SingletonBob,"favTrak4145");
+	this.hearts = ["&nbsp;&#9825;&nbsp;","&nbsp;&#9829;&nbsp;"]
+	this.isDirty= false;	this.scoreCard= {};
+	var $t= this;
+	this.start= function( par, id, len ){
+		$t.addEl( "span", par ).onclick= $t.onIconClick;
+		$t._.innerHTML = $t.i0.getIcon( $t._.asset_FavTrak= {id:id, len:len}, par );
+		return $t._;
+	}
+	this.onIconClick= function(){
+		(this._ = new FavTrak().i0).trakClick( this.asset_FavTrak );
+		var res = this._.getIcon( this.asset_FavTrak, this.parentNode );
+		this.innerHTML= res;
+	}
+	this.getIcon= function( asset, elForBg ){
+		var res=0;
+		for (var m in this.scoreCard)
+		  if (asset.id==m) res=($t.scoreCard[m]==asset.len)? 1:0;
+		return [$t.hearts[res],this.setBg(elForBg, res)][0];
+	}
+	this.setBg= function( el, isF ){
+		if (el) el.style.backgroundColor= (isF)? 'yellow':'transparent';
+	}
+	this.trakClick= function( asset ){
+		var found;
+		for (var m in this.scoreCard)
+			if (asset.id==m) found=m;
+		if ((found) && (asset.len==this.scoreCard[found])) { delete this.scoreCard[found];
+		} else this.scoreCard[asset.id]= asset.len;
+		this.isDirty = true;
+	}
+}
+//-----
+function AutoSave(){
+	(function(t,c,a){t.c=c,t.c(a)})(this,DivApp);
+	(function(t,c,a){t.c=c,t.c(a)})(this,SingletonBob);
+	this.checkInterval = 15000;	//ms
+	this.checklist = [];
+	var $t = this;
+	var $ea;
+	this.start= function(){
+		this.enabled = true;
+		this.check();
+		return this;
+	}
+	this.addClient= function( inst, propName, fileName ){
+		this.checklist.push( {inst:inst, propName:propName, fileName:fileName} );
+	}
+	this.check= function(){
+		if (!this.enabled) return;
+		for (var m in $ea=this.enumArray(this.checklist))
+			if ($ea[m].inst.isDirty) this.saveToFile( $ea[m] );
+		setTimeout( function(){$t.check()}, this.checkInterval );
+	}
+	this.saveToFile= function( pkg ){
+		var payload = JSON.stringify( pkg.inst[pkg.propName] );
+		new FsoUtil().writeFile( pkg.fileName, payload );
+		pkg.inst.isDirty= false;
+	}
+}
+//-----
+function RedButton(){
+	this.start= function( s ){
+		if (s=="Folder") return window.open(".");
+		if (s=="Reload") location.reload(true);
+		if (s=="Close") return window.close();
+		var lpn = (location.pathname).split(/[\/\\]/).pop();
+		if (s=="Spawn2") return showModelessDialog(lpn,window,"dialogwidth:1100px");
+		if (s=="Spawn") {
+			new FsoUtil().runCmd( lpn );
+			window.close();
+		}
+	}
+}
--- guptinal-i/saad/js/QuikNote.js
+++ guptinal-i/saad/js/QuikNote.js
@@ -1,4 +1,5 @@
-//4-7-2016 jchoy QuikNote, ScratchApp, RedButton, QuikLinks, AutoLink
+//4-7-2016 jchoy QuikNote, ScratchApp, RedButton, QuikLinks, AutoLink, AutoSave, FavTrak
+//https://raw.githubusercontent.com/douglascrockford/JSON-js/master/json2.js
 //-----
 function QuikNote( fn ){
 	(function(t,c){t.c=c,t.c()})(this,FormHelper);
@@ -36,8 +37,10 @@ function QuikLinks( fn ){
 	}
 	this.showPreview= function(s, el){
 		this.assetPrev = [s, el.innerHTML= ""][0];
+		var fav = [new FsoUtil().readFile("data/favtrak.txt"), new FavTrak().i0];
+		fav[1].scoreCard= JSON.parse( fav[0] );
 		new AutoLink().start( s, el, this );
-		new AutoSave().i0.start().addClient(new FavTrak().i0, "scoreCard", "favtrak.txt");
+		new AutoSave().i0.start().addClient(fav[1], "scoreCard", "data/favtrak.txt");
 	}
 	this.showEdit= function( el ){
 		var frm = this.addEl( "form", el, el.innerHTML= "" );
@@ -52,64 +55,6 @@ function QuikLinks( fn ){
 		if (this.value=="Save") 
 		  new FsoUtil().writeFile( this.form.helper.fileName, this.form.clientTa.value );
 		if (this.value=="Edit") this.form.helper.showEdit(this.form.parentNode);
-	}
-}
-//-----
-function AutoLink(){
-	(function(t,c,a){t.c=c,t.c(a)})(this,FormHelper);
-	var $ea;
-	this.start= function( s, par, mgr ){
-		this.massage(s, par);
-		var frm= this.addEl( "form", par );
-		this.addEl( "button", frm ).value="Edit";
-		this.onButtonClick= mgr.onButtonClick;
-		this.prepForm( frm );
-		frm.helper= mgr;
-	}
-	this.massage= function( s, el ){
-		for (var m in $ea=this.enumArray(s.split("\n"))){
-			this._= new FavTrak().start( this.addEl("div", el), m, $ea[m].length );
-			this.addEl("a", this._.parentNode).innerHTML = $ea[m];
-			this._.href= $ea[m];
-			this._.parentNode.style.marginTop= "6";
-		}
-	}
-}
-//-----
-function FavTrak(){
-	(function(t,c,a){t.c=c,t.c(a)})(this,FormHelper);
-	(function(t,c,a){t.c=c,t.c(a)})(this,SingletonBob,"favTrak4145");
-	this.hearts = ["&nbsp;&#9825;&nbsp;","&nbsp;&#9829;&nbsp;"]
-	this.scoreCard= {};
-	this.isDirty= false;
-	var $t= this;
-	this.start= function( par, id, len ){
-		$t.addEl( "span", par ).onclick= $t.onIconClick;
-		$t._.innerHTML = $t.i0.getIcon( $t._.asset_FavTrak= {id:id, len:len}, par );
-		return $t._;
-	}
-	this.onIconClick= function(){
-		(this._ = new FavTrak().i0).trakClick( this.asset_FavTrak );
-		var res = this._.getIcon( this.asset_FavTrak, this.parentNode );
-		this.innerHTML= res;
-	}
-	this.getIcon= function( asset, elForBg ){
-		for (var m in this.scoreCard)
-		  if (asset.id==m) {
-			var res = ($t.scoreCard[m]==asset.len)? 1:0;
-			if (elForBg) elForBg.style.backgroundColor= (res)? 'yellow':'transparent';
-			return $t.hearts[res];
-		  }
-		if (elForBg) elForBg.style.backgroundColor= 'transparent';
-		return $t.hearts[0];
-	}
-	this.trakClick= function( asset ){
-		var found;
-		for (var m in this.scoreCard)
-			if (asset.id==m) found=m;
-		if ((found) && (asset.len==this.scoreCard[found])) { delete this.scoreCard[found];
-		} else this.scoreCard[asset.id]= asset.len;
-		this.isDirty = true;
 	}
 }
 //-----
@@ -138,48 +83,7 @@ function ScratchApp(){
 		this._.cols=70;
 	}
 }
-//-----
-function AutoSave(){
-	(function(t,c,a){t.c=c,t.c(a)})(this,DivApp);
-	(function(t,c,a){t.c=c,t.c(a)})(this,SingletonBob);
-	this.checkInterval = 15000;	//ms
-	this.checklist = [];
-	var $t = this;
-	var $ea;
-	this.start= function(){
-		this.enabled = true;
-		this.check();
-		return this;
-	}
-	this.addClient= function( inst, propName, fileName ){
-		this.checklist.push( {inst:inst, propName:propName, fileName:fileName} );
-	}
-	this.check= function(){
-		if (!this.enabled) return;
-		for (var m in $ea=this.enumArray(this.checklist))
-			if ($ea[m].inst.isDirty) this.saveToFile( $ea[m] );
-		setTimeout( function(){$t.check()}, this.checkInterval );
-	}
-	this.saveToFile= function( pkg ){
-		var payload = JSON.stringify( pkg.inst[pkg.propName] );
-		new FsoUtil().writeFile( pkg.fileName, payload );
-		pkg.inst.isDirty= false;
-	}
-}
-//-----
-function RedButton(){
-	this.start= function( s ){
-		if (s=="Folder") return window.open(".");
-		if (s=="Reload") location.reload(true);
-		if (s=="Close") return window.close();
-		var lpn = (location.pathname).split(/[\/\\]/).pop();
-		if (s=="Spawn2") return showModelessDialog(lpn,window,"dialogwidth:1100px");
-		if (s=="Spawn") {
-			new FsoUtil().runCmd( lpn );
-			window.close();
-		}
-	}
-}
+
 //-----
 function LogApp(){
 	(function(t,c){t.c=c,t.c()})(this,DivApp);
