38aa10a31a08e31c60b441a4b6e5a5594a513b42
Johnston <jcgh00@okdaily.com>
2016-04-11 17:10:20 -0700

    myLayout.js and sandbox.html

---
+++ guptinal-i/saad/js/myLayout.js
@@ -0,0 +1,14 @@
+new MyPageApp().start(document.forms[0]);
+
+var plm= new PortalsLayout().getPortalsMgr();
+plm.addPortal( new QuikLinks() ).start( plm.getAvailCell(), "data/quiklink102.txt");
+plm.addPortal( new ScratchApp() ).start( plm.getAvailCell(), "scratchpad");
+plm.addPortal( new QuikNote() ).start( plm.getAvailCell(), "data/quiknote121.txt" );
+ plm.addPortal( new DomMvApp() ).start( plm.getAvailCell(), "fm101");
+ plm.addPortal( new StubApp() ).start( plm.getAvailCell(), "clock "+new Date());
+plm.addPortal( new QuikHtml() ).start( plm.getAvailCell(), "data/quikhtml121.txt");
+
+//plm.addPortal( new LogApp() ).start( plm.getAvailCell() );
+(function(toc,cl,a,plm){if(toc!='undefined')plm.addPortal(new cl()).start(plm.getAvailCell(),a)
+})( typeof LogApp, LogApp, "", new PortalsLayout().getPortalsMgr() );
+
