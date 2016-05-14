c6f117263c5154bf3a6a715654ff02526e26af14
Johnston <jcgh00@okdaily.com>
2016-05-14 11:35:34 -0700

    v0.212 textstore.htm

--- baolte-g/node-js-projects/lipaotwig/server-bum.js
+++ baolte-g/node-js-projects/lipaotwig/server-bum.js
@@ -1,3 +1,4 @@
+//5-13-2016 jchoy v0.212 textStore.htm
 //5-8-2916 jchoy server-bum.js - to use with bare nodejs on android ice cold server
 var http = require('http');
 require( './js/TextStore.js' );
@@ -6,8 +7,14 @@ var server = http.createServer(bum.reqHandler);
 var fs= require('fs');
 
 bum.addGetPath( '/vy/', function(req, res){
-    fs.writeFileSync('pdata/tstest.txt','no fs');
+    fs.writeFileSync('writedata/tstest.txt','no fs');
     bum.sendText( res, 200, "very good "+req.url )
+})
+bum.addGetPath( '/ts', function(req, res){
+    bum.sendText( res, 200, fs.readFileSync('pub_html/textStore.htm'), "text/html" );
+})
+bum.addGetPath( '/ok88.element.base.js', function(req, res){
+    bum.sendText( res, 200, fs.readFileSync('pub_html/js/ok88.element.base.js') );
 })
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
