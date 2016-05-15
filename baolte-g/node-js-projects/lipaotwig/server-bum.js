3becd53faf8e77c0e635f8b55633d66aac2ac580
Johnston <jcgh00@okdaily.com>
2016-05-14 23:36:05 -0700

    0.213 commit payload

--- baolte-g/node-js-projects/lipaotwig/server-bum.js
+++ baolte-g/node-js-projects/lipaotwig/server-bum.js
@@ -1,4 +1,4 @@
-//5-13-2016 jchoy v0.212 textStore.htm
+//5-13-2016 jchoy v0.213 commit payload
 //5-8-2916 jchoy server-bum.js - to use with bare nodejs on android ice cold server
 var http = require('http');
 require( './js/TextStore.js' );
@@ -6,9 +6,9 @@ var bum = new TextStoreWebApp();
 var server = http.createServer(bum.reqHandler);
 var fs= require('fs');
 
-bum.addGetPath( '/vy/', function(req, res){
-    fs.writeFileSync('writedata/tstest.txt','no fs');
-    bum.sendText( res, 200, "very good "+req.url )
+bum.addGetPath( '/commit/', function(req, res){
+    fs.writeFileSync( bum.getFilePath(), bum.getPayload() );
+    bum.sendText( res, 200, "Saved to  "+bum.getFilePath() );
 })
 bum.addGetPath( '/ts', function(req, res){
     bum.sendText( res, 200, fs.readFileSync('pub_html/textStore.htm'), "text/html" );
