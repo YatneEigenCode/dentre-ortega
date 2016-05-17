829eb2d8e93fbb328c779020cfbc552c0130e820
Johnston <jcgh00@okdaily.com>
2016-05-16 20:13:55 -0700

    v0.124 cookie

--- baolte-g/node-js-projects/lipaotwig/pub_html/js/RecentList.js
+++ baolte-g/node-js-projects/lipaotwig/pub_html/js/RecentList.js
@@ -1,16 +1,20 @@
-//5-16-2016 jchoy v0.121 RecentList
+//5-16-2016 jchoy v0.124 cookie
 function RecentList(){
     this.assets= [];
+    this.cookieName= "tsRecent";
     var $t=this;
     this.addItem= function(s, onclickFcn){
         this.onclickFcn= onclickFcn;
-        if (-1 == (this.assets.toString()+",").indexOf(s+","))
+        if (!s) {
+        } else if (-1 == (this.assets.toString()+",").indexOf(s+","))
             this.assets.push(s);
         this.displayList();
+        document.cookie= this.cookieName+"="+this.assets.toString();
     }
     this.displayList= function(){
         if (!this.div) this.div= this.addEl("div");
         this.div.innerHTML='';
+        this.cookieAsset();
         for (var i=0,at=this.assets; i<at.length; i++){
             this.addEl("a",this.div).innerHTML= at[i];
             this._.href= "#"+at[i];
@@ -18,6 +22,12 @@ function RecentList(){
             this._.style.padding= "0 3 0 3";
             this._.onclick= function(){$t.onclickFcn(this.payload)};
         }
+    }
+    this.cookieAsset = function( ){
+        if (this.assets.length>0) return;
+        var at= document.cookie.split( this.cookieName+"=" );
+        if (at.length<2) return;
+        this.assets= at[1].split(";")[0].split(",");
     }
     this.addEl = function( tag, par ){
         if (!par) par=document.body;
