bf565db689dc6423d0f689603b6fefe75a1e590d
Johnston <jcgh00@okdaily.com>
2016-06-01 12:08:49 -0700

    ts602.js taken from lipao ts

---
+++ baolte-g/node-js-projects/lipaotwig/pub_html/js/ts602.js
@@ -0,0 +1,9 @@
+//6-1-2016 ts602 config
+cfg= { color:'blue'
+  ,topLevel:{ sendPkgUrl: "lipaotwig.io/ts/set/?i={0}"
+             ,textPkgUrl: "/ts/text/{0}.txt"}
+}
+cfg= { color:'blue'
+  ,topLevel:{ sendPkgUrl: "/ts/set/?i={0}"
+             ,textPkgUrl: "/ts/text/?i={0}"}
+}
