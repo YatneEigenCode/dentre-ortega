cf5c2fdca3d34ce3826657aed65e805f5d67c457
Johnston <jcgh00@okdaily.com>
2016-08-07 14:33:01 -0700

    ts602.js config for local file on cruise

--- guptinal-i/jsto/cmdEngine-twigbud/js/ts602.js
+++ guptinal-i/jsto/cmdEngine-twigbud/js/ts602.js
@@ -1,4 +1,4 @@
-//7-4-2016 config for local file
+//8-7-2016 config for local file on cruise
 //6-1-2016 ts602 config
 cfg= { color:'blue'
   ,topLevel:{ sendPkgUrl: "lipaotwig.io/ts/set/?i={0}"
@@ -7,4 +7,9 @@ cfg= { color:'blue'
 xcfg= { color:'blue'
   ,topLevel:{ sendPkgUrl: "/ts/set/?i={0}"
              ,textPkgUrl: "/ts/text/?i={0}"}
+}
+//a version was used for development while on a cruise using this config
+xcfg= { color:'green'
+  ,topLevel:{ sendPkgUrl: "/ts/set/?i={0}"
+             ,textPkgUrl: "./text/{0}"}
 }
