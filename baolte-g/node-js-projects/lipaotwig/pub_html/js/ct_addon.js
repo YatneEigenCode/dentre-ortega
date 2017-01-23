db2a37b9a3a574e4a92fc30f860446103bacbb9b
Johnston <jcgh00@okdaily.com>
2017-01-22 17:03:49 -0800

    simplify

--- baolte-g/node-js-projects/lipaotwig/pub_html/js/ct_addon.js
+++ baolte-g/node-js-projects/lipaotwig/pub_html/js/ct_addon.js
@@ -1,22 +1,12 @@
-//——-
+//1-22-2017
+//===
 CmdMod03= function(cap){
   this.cap=cap;
   this.doCmd= function( at ){
-    if (at[0] == 'foo')  return 'duck';
+    if (at[0] == 'foo')  return 'hello';
     if (at[0] == 'clear') {
-      while (this.cap.rows.length>1)
-        this.cap.deleteRow(1);
+      while (this.cap.rows.length>1) this.cap.deleteRow(1);
       return this.cap.rows.length;
-    }
-    if (at[0] == 'select') {
-      var fn= function(){
-          var rng=document.createRange(), sel= window.getSelection();
-          rng.selectNodeContents(this);
-          sel.addRange( [rng, sel.removeAllRanges()][0]);
-      }
-      for (var i=1, at=this.cap.rows; i<at.length; i++)
-        at[i].cells[0].onmouseup= fn;
-      return 'ok4';
     }
   }
 }
