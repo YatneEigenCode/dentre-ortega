ec96811b116587fc154116463b63e5c75b2abc50
Johnston <jcgh00@okdaily.com>
2017-09-13 14:17:56 -0700

    v0.212 txtrail; fix else typo;

--- dufurlo-b/lipao-ts/jsplayground.js
+++ dufurlo-b/lipao-ts/jsplayground.js
@@ -1,8 +1,18 @@
-//9-12-17 v0.112 funArray and timeoutPop
+//9-13-17 v0.212 txtrail; fix else typo;
 
 funArray= function(o,n){var r=[]; while(n-->0)r.push(o); return r}
-timeoutPop= function(a,ms,x){ if(x=a.pop())x()else return;
+timeoutPop= function(a,ms,x){ if(x=a.pop())x();else return;
   setTimeout(function(){timeoutPop(a,ms)},ms);
 }
 var fn=function(){alert(3)};
-timeoutPop( funArray( fn, 3 ), 2000 );
+
+txtrail=function(el,s){
+  var f2=function(mr,t){ el.txtrailHist.push(el.innerHTML) }
+  var f1=function(amr, observer){ amr.forEach(f2); }
+  el.txtrailHist=[]; el.innerHTML=s;
+  el.onclick= function(){alert(this.txtrailHist.length)}
+  new MutationObserver(f1).observe( el, {characterData:true, subtree:true} );
+  return el;
+}
+//var el=txtrail(new AppTool().addEl("div"), "ola ");
+//timeoutPop( funArray(function(){el.innerHTML+=" *"},11), 2000 );
