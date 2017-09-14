e111161a6b61b6ed4c824cc3953796de4af112d2
Johnston <jcgh00@okdaily.com>
2017-09-13 17:49:24 -0700

    v0.231 showHist, insertAfter, popTbl;

--- dufurlo-b/lipao-ts/jsplayground.js
+++ dufurlo-b/lipao-ts/jsplayground.js
@@ -1,18 +1,27 @@
-//9-13-17 v0.212 txtrail; fix else typo;
-
+//9-13-17 v0.231 showHist, insertAfter, popTbl;
 funArray= function(o,n){var r=[]; while(n-->0)r.push(o); return r}
 timeoutPop= function(a,ms,x){ if(x=a.pop())x();else return;
   setTimeout(function(){timeoutPop(a,ms)},ms);
 }
-var fn=function(){alert(3)};
 
+insertAfter=function(nu,old){ var b=document.body; b.insertBefore(nu,old); b.insertBefore(old,nu); }
+popTbl= function(tbl,old){
+   tbl.style.border= "1 solid black";
+   insertAfter( tbl, old );
+   tbl.onclick= function(){ this.parentNode.removeChild(this); }
+   return tbl;
+}
+showHist=function(){
+   if (!this.txtrailHist) return alert("missing");
+   var tbl= popTbl(document.createElement("table"),this);
+   this.txtrailHist.forEach( function(s){tbl.insertRow(0).insertCell(0).innerHTML=s} );
+}
 txtrail=function(el,s){
-  var f2=function(mr,t){ el.txtrailHist.push(el.innerHTML) }
-  var f1=function(amr, observer){ amr.forEach(f2); }
-  el.txtrailHist=[]; el.innerHTML=s;
-  el.onclick= function(){alert(this.txtrailHist.length)}
+  var f1=function(amr, observer){ amr.forEach(function(){el.txtrailHist.push(el.innerHTML)}) }
+  el.txtrailHist=[];
+  el.onclick= showHist;
   new MutationObserver(f1).observe( el, {characterData:true, subtree:true} );
   return el;
 }
 //var el=txtrail(new AppTool().addEl("div"), "ola ");
-//timeoutPop( funArray(function(){el.innerHTML+=" *"},11), 2000 );
+//timeoutPop( funArray(function(){//  el.innerHTML=" num ["+Math.random()+"] "+el.txtrailHist.length}, 11), 2000 );
