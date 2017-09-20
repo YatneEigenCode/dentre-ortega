90a6147dc0a420dd2b136ae7dd6fed7d1c749fa6
Johnston <jcgh00@okdaily.com>
2017-09-19 17:05:38 -0700

    v0.262 txtrailSim; don't reverse array

--- dufurlo-b/lipao-ts/jsplayground.js
+++ dufurlo-b/lipao-ts/jsplayground.js
@@ -1,4 +1,4 @@
-//9-15-17 v0.254 exportNamespace
+//9-19-17 v0.262 txtrailSim; don't reverse array
 
 funArray= function(o,n){var r=[]; while(n-->0)r.push(o); return r}
 timeoutPop= function(a,ms,x){ if(x=a.pop())x();else return;
@@ -21,18 +21,21 @@ txtrail=function(el,s){
   el.onclick= function(){
     if (!this.txtrailHist) return alert("missing");
     var tbl= popTbl(document.createElement("table"),this);
-    this.txtrailHist.reverse().forEach( function(ap){ var r=tbl.insertRow(0);
-      r.insertCell(0).innerHTML=ap[0].toLocaleString(); r.insertCell(1).innerHTML=ap[1]; } );
+    this.txtrailHist.forEach( function(ap,i){ var r=tbl.insertRow(i);
+      [ap[1],ap[0].toLocaleString()].forEach(function(v){r.insertCell(0).innerHTML=v}); } );
   }
   new MutationObserver(f1).observe( el, {characterData:true, subtree:true} );
   return el;
+}
+txtrailSim=function(s){
+  for (var i=0,at=document.getElementsByTagName("div"); i<at.length; i++){if(at[i].txtrailHist)at[i].innerHTML=s}
 }
 //var el=txtrail(new AppTool().addEl("div"), "loading...");
 //timeoutPop( funArray(function(){  el.innerHTML=" num ["+Math.random()+"] "+el.txtrailHist.length}, 4), 2000 );
 
 exportNs= function(o){ for (var m in o) o[m]= eval(m); return o; }
 exportNamespace= function(){ return exportNs(
-  {txtrail:0, insertAfter:0, funArray:0, timeoutPop:0} 
+  {txtrail:0, insertAfter:0, funArray:0, timeoutPop:0, txtrailSim:0} 
 ) }
 //var nsTxtrail= txtrail();
 //var x= nsTxtrail.funArray({},9);
