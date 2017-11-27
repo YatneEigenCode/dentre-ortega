b38a42cf70be80390314d4d28e103b730a19fb43
Johnston <jcgh00@okdaily.com>
2017-11-26 21:12:17 -0800

    v0.261 count

--- baolte-g/python-projects/AndyServerWebapp/pyws/pub/js/gateway.js
+++ baolte-g/python-projects/AndyServerWebapp/pyws/pub/js/gateway.js
@@ -1,4 +1,4 @@
-//11-26-2017 jchoy v0.147 gwClientUrl
+//11-26-2017 jchoy v0.261 count
 //Widget for cross domain data read 
 //using iframe and postMessage.
 //Requires tshelper
@@ -37,27 +37,35 @@ gatewayClient= function(url, fn){
       el.src= url;
       return D.body.appendChild( el );
   })().contentWindow;
-  return gwClient(gateway, fn);
+  return gwClientUrl(gateway, url, fn);
 }
 //-----
 gwClient= function(gateway, fn){
   return gwClientUrl(gateway, '*', fn );
 }
+//-----
 gwClientUrl= function(gateway, url, fn){
+  var isBusy = false;
+  const count= {originMismatches:0,
+      requestsSent:0, messagesReceived:0}
   function handleResponse(ev){
-      try {
-        if ( url.indexOf(ev.origin) != 0 )
-          console.log("origin mismatch",ev.origin);
-      } catch (e) {}
-      if (ev.source != gateway )
-          console.log("source mismatch");
-      const gr = JSON.parse( ev.data );
-      console.log( gr );
-      if (fn) fn( gr.data );
+      count.messagesReceived++;
+      if ( (""+url).indexOf(ev.origin) != 0 )
+          return count.originMismatches++;
+      if (ev.source == gateway ){
+        const gr = JSON.parse( ev.data );
+        if (gr.cat == 'response'){
+          isBusy = false;
+          if (fn) fn( gr.data );
+        }
+      }
   }
-  window.addEventListener('message',handleResponse);
+  window.addEventListener
+    ('message',handleResponse);
   function get(nm){
       const gc = {cat:'get', num:nm}
+      isBusy = true;
+      count.requestsSent++;
       gateway.postMessage
         (JSON.stringify(gc),'*');
   }
