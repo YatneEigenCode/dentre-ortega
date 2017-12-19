986ef914eed3d2ebbda82b6ca40251c49381f42f
Johnston <jcgh00@okdaily.com>
2017-12-18 16:48:29 -0800

    v0.113 successful test on windows

--- guptinal-i/jsto/cmdEngine-twigbud/js/AniGifMaker.js
+++ guptinal-i/jsto/cmdEngine-twigbud/js/AniGifMaker.js
@@ -1,17 +1,19 @@
+//12-18-2017 v0.113 successful test on windows
 AniGifMaker=function(){
   this.start= function(){
     const ag = new Animated_GIF({repeat:null}), dim={w:320,h:240}
     ag.setSize(dim.w, dim.h);
+    ag.setDelay(0.2);
     const apt= new LinkMaker(), ian = apt.addEl('img');
     apt.wrapCloser( ian, true, 'Animated GIF' );
 
     const cnv= document.createElement('canvas');
     cnv.width= dim.w; cnv.height= dim.h;
     const ctx= cnv.getContext('2d');
-    ctx.fillStyle= 'blue';
-    for(var i = 0; i < 21; i++) {
-      ctx.fillText('Number '+i, dim.w/2, dim.h/2);
-      //ctx.fillRect(0, 0, dim.w-i*5, dim.h-i*5);
+    ctx.fillStyle= 'yellow';
+    for(var i = 0; i < 1501; i=i+10) {
+      ctx.clearRect(0, 0, dim.w, dim.h);
+      ctx.fillText(i, i $ dim.w, i % dim.h);
       const img= new Image();
       ian.src= img.src= cnv.toDataURL('image/jpeg');
       console.log( 'c', i, img.src.length );
